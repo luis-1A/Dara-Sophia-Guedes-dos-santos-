651ea99a1d6b01d4c11775a0bf7003ee5842764c
eletiva <142061648+eletiva@users.noreply.github.com>
2023-12-16 00:29:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 
+document.addEventListener('DOMContentLoaded', function() {
+    // Data do aniversário
+    const birthday = new Date('2024-03-26T00:00:00');
+
+    // Atualiza o contador a cada segundo
+    setInterval(updateCountdown, 1000);
+
+    function updateCountdown() {
+        const now = new Date();
+        const difference = birthday - now;
+
+        const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+
+        const countdownElement = document.getElementById('countdown');
+        countdownElement.innerHTML = `<p>${days} dias ${hours} horas ${minutes} minutos</p>`;
+    }
+});
